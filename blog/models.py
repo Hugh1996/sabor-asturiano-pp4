2330a215c9333089fc06d34c142dd0afbf0a1912
Hugh1996 <>
2023-12-12 22:18:37 +0000

    Add models and makemigration

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,12 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="blog_post")
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
     ingredients = models.TextField()
@@ -17,6 +19,7 @@ class Recipe(models.Model):
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes")
+    favourites = models.ManyToManyField(User, related_name="favourite")
 
     class Meta:
         ordering = ['-created_on']
@@ -27,16 +30,47 @@ class Recipe(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+
 class Comment(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    aproved= models.BooleanField(default=False)    
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}" 
+        return f"Comment {self.body} by {self.name}"
+
+
+class Rating(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    RATING_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+    rating = models.PositiveIntegerField(choices=RATING_CHOICES)
+
+    def __str__(self):
+        return f"{self.recipe} - {self.user} - {self.rating}"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = CloudinaryField(
+        "image", default="default_profile_pic.jpg")
+
+    def update_profile(self, new_bio, new_profile_picture=None):
+        self.bio = new_bio
+        if new_profile_picture:
+            self.profile_picture = new_profile_picture
+        self.save()
