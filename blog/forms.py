7abba8c94b774d32736d9d1d48cbb9888755c3e0
Hugh1996 <>
2023-12-24 16:49:34 +0000

    incorp crispy forms

--- blog/forms.py
+++ blog/forms.py
@@ -13,13 +13,12 @@ class CommentForm(forms.ModelForm):
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'slug', 'excerpt', 'ingredients',
-                   'instructions', 'featured_image']
+        fields = ['title', 'slug', 'excerpt', 'ingredients', 'instructions', 'featured_image']
 
     featured_image = forms.ImageField(required=False)
 
     def save(self, commit=True):
-        instance = super(RecipeForm, self).save(commit = False)
+        instance = super(RecipeForm, self).save(commit=False)
         instance.slug = slugify(instance.title)
         if commit:
             instance.save()
