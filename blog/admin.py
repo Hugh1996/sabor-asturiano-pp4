3eeb6c107a1ba7c29637428a454ac5056bdc1d91
Hugh1996 <>
2023-12-27 22:00:27 +0000

    Attempt django star rating

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Comment, Rating, UserProfile
+from .models import Recipe, Comment, UserProfile
 from django_summernote.admin import SummernoteModelAdmin
 
 
